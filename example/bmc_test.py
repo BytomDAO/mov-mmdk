a15dfd8a3708e24cbdff2309491dbb91d15b49cc
ipqhjjybj <250657661@qq.com>
2021-12-20 10:33:08 +0800

    add code

--- example/bmc_test.py
+++ example/bmc_test.py
@@ -33,8 +33,19 @@ def run_v3():
     print(btm_client.signed_and_send(transaction, tx_params))
 
 
+def run_test():
+    btm_client = BmcClient(address="0x2B522cABE9950D1153c26C1b399B293CaA99FcF9",
+                             private_key="1aba488300a9d7297a315d127837be4219107c62c61966ecdf7a75431d75cc61",
+                             network=EthNet.BmcTestNet.value,
+                             provider="", version=2)
+    print(btm_client.get_balance("mag"))
+    print(btm_client.get_balance("btm"))
+
+
+
 #run_v2()
-run_v3()
+#run_v3()
+run_test()
 
 
 
