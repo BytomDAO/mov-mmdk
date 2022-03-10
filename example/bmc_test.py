6eb3f8e290bfafae697e31e9bede123e307687e0
ipqhjjybj <250657661@qq.com>
2022-03-10 14:09:24 +0800

    add code

--- example/bmc_test.py
+++ example/bmc_test.py
@@ -61,16 +61,19 @@ def run_bmc_wallet_test():
                            private_key="1aba488300a9d7297a315d127837be4219107c62c61966ecdf7a75431d75cc61",
                            network=EthNet.BmcMainNet.value,
                            provider="", version=2)
-    # print(btm_client.get_cross_assets())
-    # print(btm_client.get_balance_from_bmc_wallet())
+    print(btm_client.get_cross_assets())
+    print(btm_client.get_balance_from_bmc_wallet())
     # print(btm_client.get_token_info_from_bmc_wallet("0x77197f46435d4cf8fb07953ad5ebc98ee6c8e7f1"))
     # print(btm_client.get_transaction_from_bmc_wallet("0x8B767C4A39599676CD897aB475d1CbAFE1ae1a8d", "0x09d67e0414af36bcd1276445b95a3dceafcc75c1c5a430c77ded073db9543d6e"))
-    print(btm_client.cross_to_main_chain("0x8B767C4A39599676CD897aB475d1CbAFE1ae1a8d",
-                                         "0x8e173d5115e6706b8c35f80dc005a345da98c1c0",
+    # print(btm_client.cross_to_main_chain("0x8B767C4A39599676CD897aB475d1CbAFE1ae1a8d",
+    #                                      "0x8e173d5115e6706b8c35f80dc005a345da98c1c0",
+    #                                      1))
+    print(btm_client.cross_to_main_chain("0xffffffffffffffffffffffffffffffffffffffff",
+                                         "bn1qlc9jhf00w9mqsdczu2m8ehehhp3wgd6ls5njag",
                                          1))
 
 
-run_v2()
+# run_v2()
 # run_v3()
 # run_test()
-# run_bmc_wallet_test()
+run_bmc_wallet_test()
